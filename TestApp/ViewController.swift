5d6871b6e4c031b1282e58f464fa366759c6eda1
Baccus, Jaimie (DFRC-MI)[ARCATA ASSOCIATES INC] <jbaccus@afsla15090237.ndc.nasa.gov>
2017-12-21 10:00:46 -0800

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Baccus, Jaimie (DFRC-MI)[ARCATA ASSOCIATES INC] on 12/21/17.
+//  Copyright © 2017 Baccus, Jaimie (DFRC-MI)[ARCATA ASSOCIATES INC]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
